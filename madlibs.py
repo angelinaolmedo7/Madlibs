20558834f0223e5c77641dd7295659151f2559e2
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-09-02 22:07:32 -0700

    Pronoun select
    Select pronouns for story 2, including custom pronouns

--- madlibs.py
+++ madlibs.py
@@ -25,7 +25,7 @@ def destroy(index):
 
 def user_input(prompt):
     user_input = input(prompt)
-    return user_input
+    return user_input.strip()
 
 
 #parts of speech
@@ -151,7 +151,7 @@ def blue(item):
 
 
 def purple(item):
-    return "\033[35m{}\033[00m".format(item)
+    return "\033[1;35m{}\033[00m".format(item)
 
 
 def pink(item):
@@ -167,13 +167,82 @@ def select_format():
     return user_input("Would you like to fill in the blanks in order, or randomize the order? (ORDER/RANDOM): ")
 
 
+def select_pronouns():
+    selection = user_input("Which pronuns do you use? Enter:\nS: she/her/hers\nH: he/him/his\nT: they/them/theirs\nC: custom\n[S/H/T/C]: ")
+    pronouns_dict = {
+    "they": "they",
+    "them": "them",
+    "their": "their",
+    "theirs": "theirs",
+    "ww" : "were"
+    }
+    if selection.upper() == "S":
+        pronouns_dict["they"] = "she"
+        pronouns_dict["them"] = "her"
+        pronouns_dict["their"] = "her"
+        pronouns_dict["theirs"] = "hers"
+        pronouns_dict["ww"] = "was"
+        return pronouns_dict
+    elif selection.upper() == "H":
+        pronouns_dict["they"] = "he"
+        pronouns_dict["them"] = "him"
+        pronouns_dict["their"] = "his"
+        pronouns_dict["theirs"] = "his"
+        pronouns_dict["ww"] = "was"
+        return pronouns_dict
+    elif selection.upper() == "T":
+        return pronouns_dict
+    elif selection.upper() == "C":
+        return custom_pronouns()
+    else:
+        print ("Please enter a valid selection.")
+        return select_pronouns()
+
+
+def custom_pronouns():
+    pronouns_dict = {
+    "they": "they",
+    "them": "them",
+    "their": "their",
+    "theirs": "theirs",
+    "ww" : "were"
+    }
+
+    pronouns_dict["they"] = user_input("What pronoun would you use to replace 'they' in the following sentence?\n" + purple("They") + " own(s) that jacket. ")
+    pronouns_dict["them"] = user_input("What pronoun would you use to replace 'them' in the following sentence?\nThat jacket belongs to " + purple("them") + ". ")
+    pronouns_dict["their"] = user_input("What pronoun would you use to replace 'their' in the following sentence?\nThat is " + purple("their") + " jacket. ")
+    pronouns_dict["theirs"] = user_input("What pronoun would you use to replace 'theirs' in the following sentence?\nThat jacket is " + purple("theirs") + ". ")
+    pronouns_dict["ww"] = custom_ww(pronouns_dict)
+
+    print ("\nDoes this look right?\nI saw " + purple(pronouns_dict["them"]) + " walking to the store with "+ purple(pronouns_dict["their"]) + " friend. " + purple(pronouns_dict["they"].capitalize()) + " " + purple(pronouns_dict["ww"]) + " walking a dog, but I'm not sure if it was " + purple(pronouns_dict["theirs"]) + ".")
+    yn = user_input("[YES/NO]: ")
+    if yn.upper() == "YES" or yn.upper() == "Y":
+        return pronouns_dict
+    else:
+        return custom_pronouns()
+
+
+def custom_ww(pronouns_dict): #separate function so i can call recursively if input is invalid
+    ww = user_input("Would you use 'was' or 'were' in the following sentence?\n" + pronouns_dict["they"].capitalize() + " " + purple("was/were") + " planning on getting a new jacket.\n[WAS/WERE]: ")
+    if ww.upper() == "WAS" or ww.upper() == "WERE":
+        return ww.lower()
+    else:
+        print ("Please input either WAS or WERE.")
+        return custom_ww()
+
+
 def welcome_text():
     print ("Madlibs are fun stories you fill in with your own words. We'll prompt you with parts of speech--just type your responses and hit 'ENTER'\nIf you don't know what one of the prompts means, you can enter HELP for a definition.\n\n")
 
 
-def day_at_the_beach():
+def day_at_the_beach(): #story 1
     print ("It was an average, {} day and I decided to go to the beach with {}. As we walked up to the shore, we saw a sign for {}'s Discount {}. We ran up {} to get something to eat, but were shocked to find that they only sold {}. We were already there, so we bought {}. They only cost ${} each. Carrying our purchases, we made our way down to the water {}. We put on our swimwear: {} and {} with {} to accessorize. Finally, we tried to get in the ocean. Unfortunately, it was too {} for our liking, so we went home.".format(get_adjective(),get_name(),get_name(),get_plural_noun(), get_adverb(), get_plural_noun(), get_int(), get_float(), get_adverb(), get_clothing(), get_clothing(), get_accessory(), get_adjective()))
+
+def my_wiki(): #story 2
+    select_pronouns()
 
 welcome_text()
 if select_story()==1:
     day_at_the_beach()
+else:
+    my_wiki()
