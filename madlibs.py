20d659d51c98ea6c3eb6ab31867dada2e1aed4f3
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-09-03 00:27:40 -0700

    Second story implemented
    Implemented second and final story (scaled back from 3) and cleaned up code. Tentatively finished.

--- madlibs.py
+++ madlibs.py
@@ -80,7 +80,7 @@ def get_name():
         print("A person's name, like 'Sophia' or 'Muhammad'")
         return get_name()
     else:
-        return blue(input)
+        return blue(input.lower().capitalize())
 
 
 def get_adjective():
@@ -113,17 +113,46 @@ def get_clothing():
 def get_accessory():
     input = str(user_input("Clothing accessory: "))
     if (input.upper()=="HELP"):
-        print("A nonvital, decorative item of clothing, such as 'bows', 'earrigs', or 'necklace'.\n")
+        print("A nonvital, decorative item of clothing, such as 'bows', 'earrings', or 'necklace'.\n")
         return get_accessory()
     else:
         return purple(input.lower())
+
+
+def get_food():
+    input = str(user_input("Type of food: "))
+    if (input.upper()=="HELP"):
+        print("A variety of food, such as 'pancakes', 'pizza', or 'breakfast'.\n")
+        return get_food()
+    else:
+        return purple(input.lower())
+
+
+def get_location():
+    input = str(user_input("Location: "))
+    if (input.upper()=="HELP"):
+        print("A place, such as 'grocery store' or 'dog park'.\n")
+        return get_location()
+    else:
+        return purple(input.lower())
+
+
+def get_year():
+    input = str(user_input("Year: "))
+    if (input.upper()=="HELP"):
+        print("A calendar year, such as '2019', '1802', or '300 BCE'.\n")
+        return get_year()
+    else:
+        return green(input.lower())
 #end parts of speech
 
 
 def select_story():
-    input = user_input("Which story would you like to create? (1/2/3): ")
-    if int(input) == 1 or int(input) == 2 or int(input) == 3:
+    input = user_input("Which story would you like to create? (1/2): ")
+    if str(input) == "1" or str(input) == "2":
         return int(input)
+    elif str(input).upper() == "Q":
+        return 0
     else:
         print("Invalid response")
         return select_story()
@@ -151,7 +180,7 @@ def blue(item):
 
 
 def purple(item):
-    return "\033[1;35m{}\033[00m".format(item)
+    return "\033[35m{}\033[00m".format(item)
 
 
 def pink(item):
@@ -163,7 +192,12 @@ def indigo(item):
 #end colors
 
 
-def select_format():
+def ccapitalize(item): #color capitalize
+    #print(item[5:len(item)-4].capitalize())
+    return "\0" + item[0:5] + item[5:len(item)-4].capitalize() + "\0" + item[len(item)-4:len(item)]
+
+
+def select_format(): #didn't get to this in time
     return user_input("Would you like to fill in the blanks in order, or randomize the order? (ORDER/RANDOM): ")
 
 
@@ -228,21 +262,40 @@ def custom_ww(pronouns_dict): #separate function so i can call recursively if in
         return ww.lower()
     else:
         print ("Please input either WAS or WERE.")
-        return custom_ww()
+        return custom_ww(pronouns_dict)
 
 
 def welcome_text():
-    print ("Madlibs are fun stories you fill in with your own words. We'll prompt you with parts of speech--just type your responses and hit 'ENTER'\nIf you don't know what one of the prompts means, you can enter HELP for a definition.\n\n")
+    print ("Madlibs are fun stories you fill in with your own words. We'll prompt you with parts of speech--just type your responses and hit 'ENTER'\nIf you don't know what one of the prompts means, you can enter HELP for a definition.\n")
 
 
 def day_at_the_beach(): #story 1
-    print ("It was an average, {} day and I decided to go to the beach with {}. As we walked up to the shore, we saw a sign for {}'s Discount {}. We ran up {} to get something to eat, but were shocked to find that they only sold {}. We were already there, so we bought {}. They only cost ${} each. Carrying our purchases, we made our way down to the water {}. We put on our swimwear: {} and {} with {} to accessorize. Finally, we tried to get in the ocean. Unfortunately, it was too {} for our liking, so we went home.".format(get_adjective(),get_name(),get_name(),get_plural_noun(), get_adverb(), get_plural_noun(), get_int(), get_float(), get_adverb(), get_clothing(), get_clothing(), get_accessory(), get_adjective()))
+    print ("\nA Day At The Beach\nIt was an average, {} day and I decided to go to the beach with {}. As we walked up to the shore, we saw a sign for {}'s Discount {}. We ran up {} to get something to eat, but were shocked to find that they only sold {}. We were already there, so we bought {}. They only cost ${} each. Carrying our purchases, we made our way down to the water {}. We put on our swimwear: {} and {} with {} to accessorize. Finally, we tried to get in the ocean. Unfortunately, it was too {} for our liking, so we went home.".format(get_adjective(), get_name(), get_name(), ccapitalize(get_food()), get_adverb(), get_plural_noun(), get_int(), get_float(), get_adverb(), get_clothing(), get_clothing(), get_accessory(), get_adjective()))
 
 def my_wiki(): #story 2
-    select_pronouns()
+    personal_info = {
+    "fname": user_input("What is your first name? ").lower().capitalize(),
+    "lname": user_input("What is your last name? ").lower().capitalize()
+    }
+    pronouns = select_pronouns()
+    for key in pronouns:
+        personal_info[key] = pronouns[key]
+
+    #for key in personal_info:
+    #    print(key + ": " + personal_info[key])
+
+    your_noun = get_plural_noun()
+    #story start
+    print("\nMy Wikipedia Page\n\n{} {} was born in the year {} but is now {} years old, due to inexplicable time discrepancies. {} is most well known for being the inventor of the {} and owning the largest known collection of {}.\n\nEarly Life\n\n{} shocked onlookers when {} {} born in a {} at the tender age of 0. One witness reportedly claimed that {}'s birth was 'so {} I had to look away.' Not much is known about {}'s childhood due to extensive government redaction, though there is some debate as to whether {} may have written the popular and {} novel 'The {} {}.'\n\nShort-lived Career in {} Manufacture\n\n{} {} entered the public eye when {} announced a new business venture specializing in the manufacture of {} called {}{}. Unfortunately, the business declared bankruptcy after selling only {} units and forced {} into relative obscurity. Complaints that the {} were too {} and {} were cited as the cause of {}'s downfall.".format(personal_info["fname"], personal_info["lname"], get_year(), get_int(), personal_info["lname"], get_noun(), get_plural_noun(), personal_info["lname"], personal_info["they"], personal_info["ww"], get_location(), personal_info["lname"], get_adjective(), personal_info["lname"], personal_info["they"], get_adjective(), ccapitalize(get_adjective()), ccapitalize(get_noun()), ccapitalize(your_noun), personal_info["fname"], personal_info["lname"], personal_info["they"], your_noun, ccapitalize(get_adjective()), your_noun, get_int(), personal_info["lname"], your_noun, get_adjective(), get_adjective(), personal_info["fname"]))
+    #story end
+
+
+print(ccapitalize(purple("hello")))
 
 welcome_text()
-if select_story()==1:
+
+selection = select_story()
+if selection == 1:
     day_at_the_beach()
-else:
+elif selection == 2 :
     my_wiki()
